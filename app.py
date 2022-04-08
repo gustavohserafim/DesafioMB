f75ff7430c863277d5b7f7929336f4583763652f
Gustavo Serafim <gustavo.crisostomo@linx.com.br>
2022-04-08 15:23:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+'''
+pair: BRLBTC e BRLETH // Vai no Path da url
+- from: timestamp
+- to: timestramp, default: Dia anterior
+- range: 20, 50 ou 200 (dias)
+'''
+from Controller.PairController import PairController
+
+@app.route("/<string:pair>/mms", methods=["GET"])
+def get_pair_mms(pair):
+    return PairController.get_pair_mms()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=False)
