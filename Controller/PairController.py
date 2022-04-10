fbd5ed174c555a1ace820da533420393eb47e3ac
Gustavo Serafim <tioogu@gmail.com>
2022-04-10 16:35:50 -0300

    fix: from param older than 365 validation

--- Controller/PairController.py
+++ Controller/PairController.py
@@ -11,13 +11,14 @@ class PairController:
     def get_pair_mms(param_pair, params):
         param_from, param_range = int(params["from"]), int(params["range"])
 
-        yesterday = datetime.today().date() - timedelta(days=1)
+        today = datetime.today()
+        yesterday = today.date() - timedelta(days=1)
+
         param_to = int(mktime(yesterday.timetuple())) if params["to"] is False else int(params["to"])
 
-        param_to_datetime = datetime.fromtimestamp(param_to)
-        older_than_365 = param_to_datetime < (param_to_datetime - timedelta(days=365))
+        param_from_datetime = datetime.fromtimestamp(param_from)
+        older_than_365 = param_from_datetime < (today - timedelta(days=365))
 
         if not older_than_365:
-            result = PairModel.get_pair_mms(param_pair, param_from, param_to, param_range)
-            return jsonify({"timestamp": result["timestamp"], "mms": result["mms"]}), 200
-        return jsonify({"message": "start date older than 365 days"}), 403
+            return jsonify(PairModel.get_pair_mms(param_pair, param_from, param_to, param_range)), 200
+        return jsonify({"error": "start date older than 365 days"}), 403
