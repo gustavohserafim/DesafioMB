f6925fba225d9924a83328b51446a1c5fede4a55
Gustavo Serafim <tioogu@gmail.com>
2022-04-11 01:06:26 -0300

    Update PairController.py

--- Controller/PairController.py
+++ Controller/PairController.py
@@ -20,5 +20,8 @@ class PairController:
         older_than_365 = param_from_datetime < (today - timedelta(days=365))
 
         if not older_than_365:
-            return jsonify(PairModel.get_pair_mms(param_pair, param_from, param_to, param_range)), 200
-        return jsonify({"error": "start date older than 365 days"}), 403
+            result = PairModel.get_pair_mms(param_pair, param_from, param_to, param_range)
+            if result:
+                return jsonify(result), 200
+            return jsonify({"message": "No data for the selected period.", "errors": {}}), 400
+        return jsonify({"message": "start date older than 365 days", "errors": {"from": "older than 365 days."}}), 400
