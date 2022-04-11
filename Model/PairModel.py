24a7823f47ad13c9367c0dec675da6d063421bcf
Gustavo Serafim <tioogu@gmail.com>
2022-04-11 00:08:11 -0300

    Update PairModel.py

--- Model/PairModel.py
+++ Model/PairModel.py
@@ -6,5 +6,4 @@ class PairModel:
     @staticmethod
     def get_pair_mms(p_pair, p_from, p_to, p_range):
         ranges = {20: "mms_20", 50: "mms_50", 200: "mms_200"}
-        return DB().run_fa("SELECT timestamp, {} as mms FROM pair WHERE pair = '{}' AND `timestamp` BETWEEN {} AND {};"
-                           .format(ranges[p_range], p_pair, p_from, p_to))
+        return DB().run_fa(f"SELECT timestamp, {ranges[p_range]} as mms FROM pair WHERE pair = '{p_pair}' AND `timestamp` BETWEEN {p_from} AND {p_to};")
